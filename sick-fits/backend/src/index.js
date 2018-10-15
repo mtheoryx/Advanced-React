fc166e2ad0781cac9fc17d630c5ee17721d1a4e7
David Poindexter <drpoindexter@gmail.com>
2018-10-15 11:55:19 -0400

    Get basic node and yoga server running

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,17 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// @TODO: use express middleware to handle cookies (JWT)
+// @TODO: use express middleware to populate current user
+
+
+//Start up the node server
+server.start({
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  }
+}, details => console.log(`Server is now running on port http://localhost:${details.port}`));
