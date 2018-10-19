3a7b8e9e272c5f900f02e3739faef897f5980416
David Poindexter <drpoindexter@gmail.com>
2018-10-19 13:46:12 -0400

    Adds delete function to frontend and Apollo client

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -6,6 +6,7 @@ import formatMoney from '../lib/formatMoney';
 import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
+import DeleteItem from './DeleteItem';
 
 class Item extends Component {
   static propTypes = {
@@ -29,14 +30,14 @@ class Item extends Component {
         <PriceTag>{formatMoney(item.price)}</PriceTag>
         <p>{item.description}</p>
         <div className="buttonList">
-           <Link href={{
+          <Link href={{
              pathname: 'update',
              query: { id: item.id }
-           }}>
-              <a>Edit ✏️</a>
-            </Link>
-            <button>Add To Cart</button>
-            <button>Delete</button>
+            }}>
+            <a>Edit ✏️</a>
+          </Link>
+          <button>Add To Cart</button>
+          <DeleteItem id={item.id}>Delete It</DeleteItem>
         </div>
       </ItemStyles>
     );
