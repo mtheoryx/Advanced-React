27c804a199fecab1a7bbce47281ad2cf3fb6692e
David Poindexter <drpoindexter@gmail.com>
2018-11-17 14:13:59 -0500

    Add naive example of network-only fetch policy

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -3,12 +3,15 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
 
+import { perPage } from '../config';
+
 import Item from './Item';
 import Pagination from './Pagination';
 
+
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -36,7 +39,14 @@ class Items extends Component {
     return (
       <Center>
         <Pagination page={this.props.page} />
-        <Query query={ALL_ITEMS_QUERY}>
+        <Query
+          query={ALL_ITEMS_QUERY}
+          fetchPolicy="network-only"
+          variables={{
+            skip: this.props.page * perPage - perPage,
+            first: perPage
+          }}
+        >
           {({data, error, loading}) => {
             if (loading) return <p>Loading...</p>
             if (error) return <p>Error: {error.message}</p>
