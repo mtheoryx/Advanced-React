ba1f9af8a2c300e81a7b20b6973279a8972bb2e0
David Poindexter <drpoindexter@gmail.com>
2018-11-23 14:24:59 -0500

    Handle signed in user post-signup

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
@@ -9,12 +10,25 @@ const server = createServer();
 server.express.use(cookieParser());
 
 // @TODO: use express middleware to populate current user
+// decode the JWT so we can get the user id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the user id onto the request for future reqests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 
 //Start up the node server
-server.start({
-  cors: {
-    credentials: true,
-    origin: process.env.FRONTEND_URL
-  }
-}, details => console.log(`Server is now running on port http://localhost:${details.port}`));
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  details =>
+    console.log(`Server is now running on port http://localhost:${details.port}`));
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -5,7 +5,19 @@ const {
 const Query = {
   items: forwardTo('db'),
   item: forwardTo('db'),
-  itemsConnection: forwardTo('db')
+  itemsConnection: forwardTo('db'),
+  me(parent, args, ctx, info) {
+    // Check if there is a current user Id
+    if(!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId },
+      },
+      info
+    );
+  }
 };
 
 module.exports = Query;
